74f6141e4a249c503476e2da22ad17dfcd7ee8c3
nandap <nandap@wit.edu>
2018-07-11 03:04:36 -0400

    First commit

---
+++ cmake-build-debug/CMakeFiles/doubly_linked_list.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/doubly_linked_list.dir/main.c.o"
+  "doubly_linked_list.pdb"
+  "doubly_linked_list.exe"
+  "libdoubly_linked_list.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/doubly_linked_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
